522ba6735c9bd12b94cca12c112c3cfbeb0eda94
emtiaz1 <rafid15-5151@diu.edu.bd>
2025-06-19 14:35:14 +0600

    basic structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
-    This is my portfoloio website.
-    <h1>Welcome to My Portfolio</h1>
-    <footer>
-        <p>© 2025 My Portfolio</p>
-    </footer>
+    <div class="top-bar">
+        <div class="left-items">
+            <div class='menu' type='button'>Menu</div>
+            <div class='time'>TimeDate</div>
+        </div>
+        <div class="right-item">
+            <div class="contact">Contact →</div>
+        </div>
+    </div>
+    <div class="main-section">
+        <div class="profile">Profile Pic</div>
+        <div class="welcome">
+    <h2>Welcome to My Portfolio!</h2>
+    <p>
+        I'm Emtiaz Ahmed, and I'm excited to share my work with you. This portfolio showcases the projects, skills, and ideas I've developed over time — from web development and programming to research and innovation.
+    </p>
+    <p>
+        Feel free to explore, learn more about what I do, and get in touch if you'd like to collaborate or just have a chat. Thanks for stopping by!
+    </p>
+    </div>
+    </div>
+
 </body>
 </html>
